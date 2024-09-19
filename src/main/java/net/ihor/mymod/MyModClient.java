9dd67f94e6b52b86186398a13a7961b951d07005
Ihor <igorskor0912@gmail.com>
2024-09-19 04:42:45 +0300

    Init commit

---
+++ src/main/java/net/ihor/mymod/MyModClient.java
@@ -0,0 +1,10 @@
+package net.ihor.mymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ihor/mymod/MyModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ihor.mymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
